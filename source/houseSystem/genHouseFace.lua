0c8d25424ea3c1455f1f31d335bfc28083dfe890
Chrystian <chrystian.ldv@gmail.com>
2020-08-19 11:49:23 -0300

    Quest 5 integration

--- source/houseSystem/genHouseFace.lua
+++ source/houseSystem/genHouseFace.lua
@@ -31,7 +31,7 @@ HouseSystem.genHouseFace = function(self, guest)
 					function(player)
 						furniture.usable(player)
 					end)
-				if furniture.name == 'chest' then
+				if furniture.name == 'chest' or furniture.name == 'freezer' then
 					players[self.houseOwner].houseData.chests.position[furniture.uniqueID] = {x = x, y = y}
 				end
 			end
