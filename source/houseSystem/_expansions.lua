344f5c6ed7761ae226634b150102f1b35c4d1f76
Chrystian <chrystian.ldv@gmail.com>
2020-09-03 18:50:19 -0300

    Changing garden design

--- source/houseSystem/_expansions.lua
+++ source/houseSystem/_expansions.lua
@@ -19,8 +19,7 @@ HouseSystem.expansions = {
 		add = function(owner, y, terrainID, plotID, guest)
 			if players[owner].houseTerrainPlants[plotID] == 0 then players[owner].houseTerrainPlants[plotID] = 1 end
 			local stage = HouseSystem.plants[players[owner].houseTerrainPlants[plotID]].stages[players[owner].houseTerrainAdd[plotID]]
-			room.houseImgs[terrainID].expansions[#room.houseImgs[terrainID].expansions+1] = addImage('16bf5b9e800.jpg', '_99991', (terrainID-1)*1500+738-(175/2) + (plotID-1)*175, y+170-45, guest)
-			room.houseImgs[terrainID].expansions[#room.houseImgs[terrainID].expansions+1] = addImage(stage, '!200000', (terrainID-1)*1500+738-(175/2) + (plotID-1)*175, y+170-45-280, guest)
+			room.houseImgs[terrainID].expansions[#room.houseImgs[terrainID].expansions+1] = addImage(stage, '!200000', (terrainID-1)*1500+738-(175/2) + (plotID-1)*175, y+170-45-275, guest)
 			if owner == 'Oliver' then
 				room.houseImgs[terrainID].expansions[#room.houseImgs[terrainID].expansions+1] = addImage(stage, '?10', 11330 + (plotID-1)*65, 7470+30, guest)
 			end
