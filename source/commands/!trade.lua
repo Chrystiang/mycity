df680b08593d6eb38aae706ef7e4a770f5c71df7
Chrystian <chrystian.ldv@gmail.com>
2020-09-03 18:50:50 -0300

    Small changes

--- source/commands/!trade.lua
+++ source/commands/!trade.lua
@@ -4,11 +4,6 @@ chatCommands.trade = {
 		local player1 = string.nick(args[1]) or player
 		local player2 = string.nick(args[2])
 		if not player2 then return end
-
-		modernUI.new(player2, 240, 170, translate('trade_invite_title', player2), translate('trade_invite', player2):format(player1))
-		:build()
-		:addConfirmButton(function(player2) 
-			tradeSystem.new(player1, player2)
-		end, translate('submit', player2), player1)
+		tradeSystem.invite(player1, player2)
 	end
 }
