67b3352bf3e3a3f5e8e9e9728b9b4bb41af0fc61
Chrystian <chrystian.ldv@gmail.com>
2020-09-17 16:19:29 -0300

    Added the possibility to ban people even if they are not in the room

--- source/commands/!ban.lua
+++ source/commands/!ban.lua
@@ -1,8 +1,7 @@
 chatCommands.ban = {
 	permissions = {'admin', 'mod'},
 	event = function(player, args)
-		local target = string.nick(args[1])
-		if not players[target] then return TFM.chatMessage('<g>[•] $playerName not found.', player) end
+		local target = string.nick(args[1]) or args[1]
 		room.bannedPlayers[#room.bannedPlayers+1] = target
 		TFM.killPlayer(target)
 		translatedMessage('playerBannedFromRoom', target)
