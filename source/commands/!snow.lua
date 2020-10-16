e4c32d0008802ff0bcc612c386d379516bd97da7
Blank3495 <49693827+Blank3495@users.noreply.github.com>
2020-10-15 21:38:19 -0300

    fix snow command
    the command accepts any args, even strings, which means that if anyone uses the command to snow for only 5 seconds, it would never end.

--- source/commands/!snow.lua
+++ source/commands/!snow.lua
@@ -1,9 +1,12 @@
 chatCommands.snow = {
 	permissions = {'admin', 'mod', 'helper'},
 	event = function(player, args)
-		local duration = args[1] or 0
-		local snowballPower = args[2] or 10
-		TFM.snow(duration, snowballPower)
-		TFM.chatMessage('<g>[•] Done! Snowing for '..duration..', snow ball power: '..snowballPower, player) 
+		local duration = tonumber(args[1]) or 0
+		if duration > 0 then
+			TFM.snow(duration, snowballPower)
+			TFM.chatMessage('<g>[•] Done! Snowing for '..duration.., player)
+		else
+			TFM.chatMessage('<g>[•] Invalid syntax', player)
+		end
 	end
 }
