6137b73f325d2346869914319fd22516e2a3348d
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2021-12-19 18:54:06 -0300

    Update PlayerRespawn.lua

--- source/events/PlayerRespawn.lua
+++ source/events/PlayerRespawn.lua
@@ -5,14 +5,14 @@ onEvent("PlayerRespawn", function(player)
 	if not players[player] then return end
 	if not players[player].dataLoaded then return end
 	if isExploiting[player] then return end
-	--[[
-	if lastRevive[player] then
+	
+	--[[if lastRevive[player] then
 		if lastRevive[player] > os_time()-10000 and not players[player].editingHouse then
 			isExploiting[player] = true
 			return
 		end
-	end
-	lastRevive[player] = os_time()]]
+	end]]
+	lastRevive[player] = os_time()
 
 	local level = players[player].level[1]
 	for i, v in next, ROOM.playerList do
