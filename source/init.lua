57a3bbcd7a3f80f1df160c70b49419438850c43a
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2021-12-20 11:12:47 -0300

    Christmas 2021

--- source/init.lua
+++ source/init.lua
@@ -27,8 +27,67 @@ startRoom = function()
 				for player in next, ROOM.playerList do
 					--players[player].timePlayed = players[player].timePlayed + 1/60/60
 					updateBarLife(player)
+					TFM.snow(1000, 10)
 				end
 			end, 60000, 0)
+
+			local totalOfSpawnedGifts = 0
+			addTimer(function()
+				if room.isInLobby then return end
+				local giftPosition = random(1, #room.giftsPositions)
+				local giftsLeft = 3
+				local giftId = 1000 + totalOfSpawnedGifts
+
+				local allImages = {}
+				local playerImages = {}
+
+				giftPosition = room.giftsPositions[giftPosition]
+				TouchSensor.add(
+					0,
+					giftPosition.x,
+					giftPosition.y-20,
+					giftId,
+					0,
+					false,
+					0,
+					function(player)
+						if giftsLeft > 0 then
+							giftsLeft = giftsLeft - 1
+
+							modernUI.new(player, 120, 120)
+							:build()
+							players[player]._modernUISelectedItemImages[1][#players[player]._modernUISelectedItemImages[1]+1] = addImage(bagItems['goldenPresent'].png, ":70", 400 - 50 * 0.5, 180, player)
+
+							addItem('goldenPresent', 1, player)
+							job_updatePlayerStats(player, 21, 1)
+
+							removeImage(playerImages[player])
+							if giftsLeft == 0 then
+								TouchSensor.remove(giftId)
+								removeGroupImages(allImages)
+							end
+						end
+					end,
+					false
+				)
+				for player in next, ROOM.playerList do
+					local _img = addImage('17dd48d1819.png', "!10", giftPosition.x-15, giftPosition.y-15-20, player)
+
+					allImages[#allImages+1] = _img
+					playerImages[player] = _img
+
+					TouchSensor.add(
+						0,
+						giftPosition.x,
+						giftPosition.y-20,
+						giftId,
+						0,
+						false,
+						player
+					)
+				end
+				totalOfSpawnedGifts = totalOfSpawnedGifts + 1
+			end, 10 * 60 * 1000, 0)
 		end
 	end
 end
@@ -84,6 +143,7 @@ genDaveOffers()
 
 for item, data in next, Mine.ores do 
 	bagItems['crystal_'..item].price = floor(200*(12/data.rarity))
+	bagItems['crystal_'..item].sellingPrice = floor(200*(12/data.rarity))
 end
 mine_generate()
 
@@ -113,7 +173,6 @@ for place, data in next, places do
 				players[player].place = placeToGo
 
 				checkIfPlayerIsDriving(player)
-				showOptions(player)
 				if placeToGo == 'mine' or placeToGo == 'mine_excavation' then
 					setNightMode(player, false)
 				else
@@ -172,9 +231,9 @@ end, 10)
 
 loadFile(1)
 
-local lastFile = 5
+local lastFile = 30
 addTimer(function()
-	if lastFile == 5 then
+	if lastFile == 30 then
 		if room.fileUpdated then
 			syncFiles()
 			room.fileUpdated = false
@@ -183,8 +242,8 @@ addTimer(function()
 		end
 		lastFile = 1
 	elseif lastFile == 1 then
-		loadFile(5)
-		lastFile = 5
+		loadFile(30)
+		lastFile = 30
 	end
 end, 15000, 0)
 
