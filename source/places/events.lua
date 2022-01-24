fd34534f4246e2bda30bc827f5739a9dc1e44e15
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2022-01-24 11:12:30 -0300

    V3.7.0

--- source/places/events.lua
+++ source/places/events.lua
@@ -60,20 +60,3 @@ leaveHouse = function(player, terrainID)
 	end
 	room.terrains[terrainID].guests[player] = false
 end
-
-equipHouse = function(player, houseType, terrainID)
-	if room.terrains[terrainID].owner then return alert_Error(player, 'error', 'alreadyLand') end
-	if players[player].houseData.houseid > 0 then
-		HouseSystem.new(player):removeHouse()
-	end
-	removeTextArea(24+terrainID)
-	removeTextArea(44+terrainID)
-	removeTextArea(terrainID)
-	players[player].houseData.houseid = terrainID
-	players[player].houseData.currentHouse = houseType
-	room.terrains[terrainID].bought = true
-	room.terrains[terrainID].owner = player
-	room.terrains[terrainID].settings = {isClosed = false, permissions = {[player] = 4}}
-
-	HouseSystem.new(player):genHouseFace():genHouseGrounds()
-end
