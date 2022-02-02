c9e90c36f515de466e8eb319878bbb3c6b1c3fb8
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2022-02-02 10:42:41 -0300

    Update showHouses.lua

--- source/interfaces/modernUI/showHouses.lua
+++ source/interfaces/modernUI/showHouses.lua
@@ -1,31 +1,39 @@
 modernUI.showHouses = function(self, selectedTerrain)
-	local id = self.id
-	local player = self.player
-	local width = self.width
-	local height = self.height
-	local x = (400 - width/2) + 20
-	local y = (200 - height/2) + 65
+	local id 		= self.id
+	local player 	= self.player
+	local width 	= self.width
+	local height 	= self.height
+	local x 		= (400 - width/2) + 20
+	local y 		= (200 - height/2) + 65
+	local i 		= 0
+
 	players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('172763e41e1.jpg', ":27", x+337, y-14, player)
-	local i = 0
+	
 	for _ = 1, #mainAssets.__houses do
 		local v = mainAssets.__houses[_]
 		local isLimitedTime = v.properties.limitedTime
-		local isOutOfSale = isLimitedTime and formatDaysRemaining(isLimitedTime, true)
-		local showItem = true
+		local isOutOfSale 	= isLimitedTime and formatDaysRemaining(isLimitedTime, true)
+		local showItem 		= true
+
 		if isLimitedTime and (isOutOfSale and not table_find(players[player].houses, _)) then
 			showItem = false
 		end
+
 		if v.properties.requires and showItem then
 			showItem = v.properties.requires(player)
 		end
+
 		if showItem then
 			i = i + 1
+
 			local image = v.properties.png or '16c25233487.png'
 			players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage('1722d2d8234.jpg', ":26", x + ((i-1)%5)*63, y + floor((i-1)/5)*65, player)
 			players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage(image, ":26", x + 5 + ((i-1)%5)*63, y + 5 + floor((i-1)/5)*65, player)
+			
 			if isLimitedTime and not isOutOfSale then
 				showTextArea(id..(895+i*2), '<p align="center"><font size="9"><r>'..translate('daysLeft2', player):format(formatDaysRemaining(isLimitedTime)), player, x + 3 + ((i-1)%5)*63, y + 49 + floor((i-1)/5)*65, 55, nil, 0xff0000, 0xff0000, 0, true)
 			end
+
 			showTextArea(id..(896+i*2), '\n\n\n\n', player, x + 3 + ((i-1)%5)*63, y + 3 + floor((i-1)/5)*65, 55, 55, 0xff0000, 0xff0000, 0, true,
 				function(player, i)
 					local itemName = translate('House'.._, player)
@@ -55,9 +63,10 @@ modernUI.showHouses = function(self, selectedTerrain)
 						showTextArea(id..(933+i*5), '<p align="center"><font color="#cef1c3" size="13">'..text..'\n', player, x-4, y-4, width+8, height+8, 0xff0000, 0xff0000, 0, true, not blockClick and callback or nil)
 					end
 
-					local buttonType = nil
-					local blockClick = false
-					local buttonAction = nil
+					local buttonType 	= nil
+					local blockClick 	= false
+					local buttonAction 	= nil
+
 					if table_find(players[player].houses, _) then
 						buttonType =  translate('use', player)
 						buttonAction = 'use'
@@ -75,7 +84,10 @@ modernUI.showHouses = function(self, selectedTerrain)
 						function(player)
 							if buttonAction == 'use' then
 								eventTextAreaCallback(0, player, 'modernUI_Close_'..id, true)
-								equipHouse(player, _, selectedTerrain)
+								modernUI.new(player, 310, 280, translate('chooseSaveSlot', player))
+									:build()
+										:showHouseSlotsToChoose(_, selectedTerrain)
+
 							elseif buttonAction == 'buy' then
 								if room.terrains[selectedTerrain].owner then return alert_Error(player, 'error', 'alreadyLand') end
 								if table_find(players[player].houses, _) then return end
@@ -90,8 +102,6 @@ modernUI.showHouses = function(self, selectedTerrain)
 								modernUI.new(player, 120, 120)
 								:build()
 								players[player]._modernUISelectedItemImages[1][#players[player]._modernUISelectedItemImages[1]+1] = addImage(v.properties.png, "&70", 400 - 50 * 0.5, 180, player)
-
-								equipHouse(player, _, selectedTerrain)
 							end
 						end, 507, 295, 120, 13, blockClick)
 				end, i)
