15697a18f537c7abcea6bdc2464a5b6ed053b4a3
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2022-03-03 17:03:41 -0300

    Update showMill.lua

--- source/interfaces/modernUI/showMill.lua
+++ source/interfaces/modernUI/showMill.lua
@@ -10,12 +10,12 @@ modernUI.showMill = function(self)
 		local amount = i > 1 and i*i or i
 		local amount2 = amount*5
 
-		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage("1717eaef706.png", ":20", 288, y+65 + (i-1)*45, player) -- Background
+		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage("1717eaef706.png", "~20", 288, y+65 + (i-1)*45, player) -- Background
 
-		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('1717f0a6947.png', ":21", 315, y+66 + (i-1)*45, player) -- Item background1
-		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage(bagItems.wheat.png, ":22", 310, y+60 + (i-1)*45, player) -- Required Item Image
+		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('1717f0a6947.png', "~21", 315, y+66 + (i-1)*45, player) -- Item background1
+		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage(bagItems.wheat.png, "~22", 310, y+60 + (i-1)*45, player) -- Required Item Image
 
-		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('171830fd281.png', ":25", 377, y+70 + (i-1)*45, player) -- Arrow
+		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('171830fd281.png', "~25", 377, y+70 + (i-1)*45, player) -- Arrow
 
 		showTextArea(id..(900+(i-1)*3), '<p align="right"><b><font size="9"><j>x<font size="12"><vp>'..amount, player, 447, y+86 + (i-1)*45, 30, nil, 0xff0000, 0xff0000, 0, true)
 
@@ -31,10 +31,10 @@ modernUI.showMill = function(self)
 				end)
 		else
 			showTextArea(id..(901+(i-1)*3), '<p align="right"><b><font size="9"><j>x<font size="12"><r>'..amount2, player, 322, y+86 + (i-1)*45, 30, nil, 0xff0000, 0xff0000, 0, true)
-			players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage("1717eaef706.png", ":30", 288, y+65 + (i-1)*45, player)
+			players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage("1717eaef706.png", "~30", 288, y+65 + (i-1)*45, player)
 		end
-		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('1717f0a6947.png', ":23", 440, y+66 + (i-1)*45, player) -- Item background1
-		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage(bagItems.wheatFlour.png, ":24",	435, y+60 + (i-1)*45, player) -- Final Item Image
+		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('1717f0a6947.png', "~23", 440, y+66 + (i-1)*45, player) -- Item background1
+		players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage(bagItems.wheatFlour.png, "~24",	435, y+60 + (i-1)*45, player) -- Final Item Image
 	end
 	return setmetatable(self, modernUI)
 end
