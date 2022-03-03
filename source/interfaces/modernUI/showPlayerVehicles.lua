674b200d84e7f53ee5dfdc30a96b668eeda4b000
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2022-03-03 17:03:33 -0300

    Update showPlayerVehicles.lua

--- source/interfaces/modernUI/showPlayerVehicles.lua
+++ source/interfaces/modernUI/showPlayerVehicles.lua
@@ -9,7 +9,7 @@ modernUI.showPlayerVehicles = function(self)
 	local filter = {'car', 'boat', 'air'}
 	local pages = {'land', 'water', 'air'}
 	local favorites = players[player].favoriteCars
-	players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('17238027fa4.jpg', ":26", x, y-16, player)
+	players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage('17238027fa4.jpg', "~26", x, y-16, player)
 
 	local function showItems()
 		showTextArea(id..'890', '<font color="#ebddc3" size="13"><b><p align="center">'..translate(pages[currentPage]..'Vehicles', player), player, 350, y-25, 100, nil, 0x152d30, 0x152d30, 0, true)
@@ -17,12 +17,12 @@ modernUI.showPlayerVehicles = function(self)
 		for _ = 1, #players[player].cars do 
 			local v = mainAssets.__cars[players[player].cars[_]]
 			if v.type == filter[currentPage] then 
-				players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage('17237e4b350.jpg', ":26", x + ((i-1)%4)*107, y + floor((i-1)/4)*65, player)
-				players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage(v.icon, ":26", x + ((i-1)%4)*107, y + floor((i-1)/4)*65, player)
-				players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage('17958659424.png', ":27", x + ((i-1)%4)*107, y + floor((i-1)/4)*65, player)
+				players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage('17237e4b350.jpg', "~26", x + ((i-1)%4)*107, y + floor((i-1)/4)*65, player)
+				players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage(v.icon, "~26", x + ((i-1)%4)*107, y + floor((i-1)/4)*65, player)
+				players[player]._modernUISelectedItemImages[3][#players[player]._modernUISelectedItemImages[3]+1] = addImage('17958659424.png', "~27", x + ((i-1)%4)*107, y + floor((i-1)/4)*65, player)
 
 				local isFavorite = favorites[currentPage] == players[player].cars[_] and '17238fea420.png' or '17238fe6532.png'
-				players[player]._modernUISelectedItemImages[1][#players[player]._modernUISelectedItemImages[1]+1] = addImage(isFavorite, ":27", x+82 + ((i-1)%4)*107, y +42 + floor((i-1)/4)*65, player)
+				players[player]._modernUISelectedItemImages[1][#players[player]._modernUISelectedItemImages[1]+1] = addImage(isFavorite, "~27", x+82 + ((i-1)%4)*107, y +42 + floor((i-1)/4)*65, player)
 
 				local vehicleName = lang.en['vehicle_'..players[player].cars[_]] and translate('vehicle_'..players[player].cars[_], player) or v.name
 				showTextArea(id..(895+i*4), '<p align="center"><font color="#95d44d" size="10">'..vehicleName, player, x + ((i-1)%4)*107, y-2 + floor((i-1)/4)*65, 104, nil, 0xff0000, 0xff0000, 0, true)
@@ -71,9 +71,9 @@ modernUI.showPlayerVehicles = function(self)
 			end)
 
 		removeGroupImages(players[player]._modernUISelectedItemImages[2])
-		players[player]._modernUISelectedItemImages[2][#players[player]._modernUISelectedItemImages[2]+1] = addImage('172380798d8.jpg', ":26", x, y+205, player)
+		players[player]._modernUISelectedItemImages[2][#players[player]._modernUISelectedItemImages[2]+1] = addImage('172380798d8.jpg', "~26", x, y+205, player)
 		for i = 1, 3 do 
-			players[player]._modernUISelectedItemImages[2][#players[player]._modernUISelectedItemImages[2]+1] = addImage('172383aa660.jpg', ":27", x + (i-1)*85 + (currentPage-1)*85, y+205, player)
+			players[player]._modernUISelectedItemImages[2][#players[player]._modernUISelectedItemImages[2]+1] = addImage('172383aa660.jpg', "~27", x + (i-1)*85 + (currentPage-1)*85, y+205, player)
 		end
 	end
 
