7ad0e75fe7115c073f01d068b70e66e34bdc1563
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2022-03-03 15:51:37 -0300

    Update setNightMode.lua

--- source/social/setNightMode.lua
+++ source/social/setNightMode.lua
@@ -1,11 +1,11 @@
 setNightMode = function(name, remove)
 	if not players[name].isBlind and remove then return end
-	if not remove then 
-		players[name].isBlind = {}
-		players[name].isBlind[#players[name].isBlind+1] = addImage("1721ee7d5b9.png", '$'..name, -800, -400, name)
+	if not remove then
+		setPlayerNightMode(name, true)
 		showTextArea(1051040, '', name, 800, 8400, 400, 700, 0x1, 0x1, 1)
+		players[name].isBlind = true
 	else
-		removeGroupImages(players[name].isBlind)
+		setPlayerNightMode(name, false)
 		removeTextArea(1051040, name)
 		players[name].isBlind = false
 	end
