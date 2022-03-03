41b945d59e00d4b39a684f3bfafc57ec011bc90c
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2022-03-03 17:03:39 -0300

    Update showNewLevelOrb.lua

--- source/interfaces/modernUI/showNewLevelOrb.lua
+++ source/interfaces/modernUI/showNewLevelOrb.lua
@@ -4,7 +4,7 @@ modernUI.showNewLevelOrb = function(self, orb)
 	local x = (400 - 220/2)
 	local y = (200 - 90/2) - 30
 
-	players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage(mainAssets.levelIcons.star[orb], ":70", 370, 145, player)
+	players[player]._modernUIImages[id][#players[player]._modernUIImages[id]+1] = addImage(mainAssets.levelIcons.star[orb], "~70", 370, 145, player)
 	showTextArea(id..'890', '<p align="center"><vp>'..translate('newLevelOrb_info', player), player, x+10, y+90, 200, nil, 0, 0x24474, 0, true)
 
 	return setmetatable(self, modernUI)
