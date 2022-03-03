7c6c0fe53d7a81a38b1840fcbfedb987a97583b2
Chrystian <37307204+Chrystiang@users.noreply.github.com>
2022-03-03 17:02:46 -0300

    Update setNightMode.lua

--- source/social/setNightMode.lua
+++ source/social/setNightMode.lua
@@ -1,12 +1,6 @@
 setNightMode = function(name, remove)
 	if not players[name].isBlind and remove then return end
-	if not remove then
-		setPlayerNightMode(name, true)
-		showTextArea(1051040, '', name, 800, 8400, 400, 700, 0x1, 0x1, 1)
-		players[name].isBlind = true
-	else
-		setPlayerNightMode(name, false)
-		removeTextArea(1051040, name)
-		players[name].isBlind = false
-	end
+
+	setPlayerNightMode(name, not remove)
+	players[name].isBlind = not remove
 end
